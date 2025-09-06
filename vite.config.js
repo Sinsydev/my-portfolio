3d6f9187029d060c1a576e5572df2416f60ea6c0
sinsydev <sinsyfrontenddeveloper@gmail.com>
2025-09-06 19:45:34 +0200

    fixed file

--- vite.config.js
+++ vite.config.js
@@ -5,17 +5,15 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   build: {
-    
     chunkSizeWarningLimit: 1000,
-
     rollupOptions: {
       output: {
         manualChunks: {
           react: ["react", "react-dom"],
-          vendor: ["axios", "lodash"]
-        }
-      }
-    }
-  }
+          vendor: ["axios"],
+        },
+      },
+    },
+  },
 });
 
