a3aa29300c5bca93689c07a9f0b1fea06660daa9
sinsydev <sinsyfrontenddeveloper@gmail.com>
2025-09-08 19:00:08 +0200

    file fixed

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,44 @@
  // vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { visualizer } from "rollup-plugin-visualizer";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    visualizer({
+      filename: "dist/stats.html", 
+      template: "treemap",         
+      gzipSize: true,
+      brotliSize: true,
+      open: true,                 
+    }),
+  ],
   build: {
     chunkSizeWarningLimit: 1000,
     rollupOptions: {
       output: {
-        manualChunks: {
-          react: ["react", "react-dom"],
-          vendor: ["axios"],
+        manualChunks(id) {
+          if (id.includes("node_modules/react") || id.includes("node_modules/react-dom")) {
+            return "react";
+          }
+          if (id.includes("node_modules/axios")) {
+            return "axios";
+          }
+          if (id.includes("node_modules/react-router")) {
+            return "react-router";
+          }
+          if (id.includes("node_modules/redux") || id.includes("node_modules/@reduxjs")) {
+            return "redux";
+          }
+          if (id.includes("node_modules")) {
+            return "vendor";
+          }
         },
       },
     },
   },
 });
+
+
 
