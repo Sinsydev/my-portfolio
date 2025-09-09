5836624c62d7098bac5f5cb362986c3c91909372
sinsydev <sinsyfrontenddeveloper@gmail.com>
2025-09-09 21:20:15 +0200

    fixed file

--- node_modules/three/examples/jsm/renderers/common/Info.js
+++ node_modules/three/examples/jsm/renderers/common/Info.js
@@ -4,12 +4,22 @@ class Info {
 
 		this.autoReset = true;
 
+		this.frame = 0;
+		this.calls = 0;
+
 		this.render = {
-			frame: 0,
+			calls: 0,
 			drawCalls: 0,
 			triangles: 0,
 			points: 0,
-			lines: 0
+			lines: 0,
+			timestamp: 0
+		};
+
+		this.compute = {
+			calls: 0,
+			computeCalls: 0,
+			timestamp: 0
 		};
 
 		this.memory = {
@@ -47,12 +57,23 @@ class Info {
 
 	}
 
+	updateTimestamp( type, time ) {
+
+		this[ type ].timestamp += time;
+
+	}
+
 	reset() {
 
 		this.render.drawCalls = 0;
+		this.compute.computeCalls = 0;
+
 		this.render.triangles = 0;
 		this.render.points = 0;
 		this.render.lines = 0;
+
+		this.render.timestamp = 0;
+		this.compute.timestamp = 0;
 
 	}
 
@@ -60,8 +81,13 @@ class Info {
 
 		this.reset();
 
-		this.render.frame = 0;
+		this.calls = 0;
 
+		this.render.calls = 0;
+		this.compute.calls = 0;
+
+		this.render.timestamp = 0;
+		this.compute.timestamp = 0;
 		this.memory.geometries = 0;
 		this.memory.textures = 0;
 
