7dd2cfd1d725a7ce010fdcf46652997e31dbbad8
sinsydev <Ismailaminusaid1234@gmail.com>
2025-09-24 21:10:48 +0200

    initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
-import { defineConfig } from "vite";
+ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { visualizer } from "rollup-plugin-visualizer";
 import path from "path";
 
 export default defineConfig({
-  base: "./", 
+  // 👇 base set to "./" so Netlify loads JS/CSS correctly
+  base: "./",
   plugins: [
     react(),
     visualizer({
@@ -51,5 +52,11 @@ export default defineConfig({
       },
     },
   },
+  
+  server: {
+    port: 3000,
+    open: true,
+  },
 });
+
 
