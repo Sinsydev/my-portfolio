bad0777dea4f10885166771a6249aa4fcf611413
sinsydev <Ismailaminusaid1234@gmail.com>
2025-10-05 19:03:29 +0200

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,7 @@ import { About, Contact, Experience, Hero, Navbar, StarsCanvas, Tech, Works } fr
 
 const App = () => {
   return (
-    <HashRouter
-      future={{
-        v7_startTransition: true,
-        v7_relativeSplatPath: true,
-      }}
-    >
+    <HashRouter>
       <div className="relative z-0 bg-primary">
         <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
           <Navbar />
